2da2cf6d7283f73d1fd402b16a186c709577cd94
SatishKumar A <satishkumar.a@gmail.com>
2021-02-10 13:37:44 +0100

    V1.0.5

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  //const name = process.env.NAME || 'World';
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  //res.send(`Hello ${name}!`);
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
+  console.log(`Application: listening on port ${port}`);
 });
