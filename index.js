17878883c58178e659596e47dcd73208472622ee
SatishKumar A <satishkumar.a@gmail.com>
2021-02-10 14:28:55 +0100

    V1.0.7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/', express.static(path.join(__dirname, 'build', 'index.html')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   //const name = process.env.NAME || 'World';
