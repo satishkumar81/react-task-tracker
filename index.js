9fe159fd7fd96c69264fa7254fb7e9c73aacfda4
SatishKumar A <satishkumar.a@gmail.com>
2021-02-10 14:18:47 +0100

    V1.0.6

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/', express.static(path.join(__dirname, 'build', 'index.html')));
+
 app.get('/', (req, res) => {
   //const name = process.env.NAME || 'World';
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
